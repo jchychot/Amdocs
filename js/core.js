c042c75b01d86d48d84a3067267a5f21e3e37607
Larry Poon <lpoon2@illinois.edu>
2016-06-22 16:40:03 -0500

    implemented user-email-specified list of CCs, auto-fill CCs function in progress

--- js/core.js
+++ js/core.js
@@ -1,16 +1,20 @@
-// Service
 angular.module('rfcService', [])
 
-.factory('RFC_factory', ['$http', function($http, $q){
+.factory('RFC_factory', ['$http','$q', function($http, $q){
 var authorizationResult = false;
 return{
-  create : function(data){
+  create_form : function(data){
       return $http.post('/api/rfc', data);
   },
-
+  create_form2 : function(data){
+      return $http.post('/api/rfc/', data);
+  },
   get : function() {
       return $http.get('/api/rfc');
 },
+  modify_form : function(data){
+    return $http.put('/api/rfc', data);
+  },
 
         initialize: function() {
             //initialize OAuth.io with public key of the application
@@ -52,30 +56,61 @@ return{
 // controller
 angular.module('rfcController',[])
 
-.controller('mainController', ['$scope', '$http','RFC_factory','$q',  function($scope, $http,$q, RFC_factory){
+.controller('mainController', ['$scope', '$http','RFC_factory','$q',  function($scope, $http, RFC_factory,$q){
 $scope.request = {};
 
 //initialize twitter
 RFC_factory.initialize();
+//RFC_factory.initialize();
   // create entry
   $scope.createRFC = function(){
-		alert($('#username').html());
 
   $scope.request.name= $('#username').html();
-  $scope.request.email = email;
-     $scope.request.impact = $('input[name="optradio"]:checked').val();
+  $scope.request.email = email2;
+  $scope.request.impact = $('input[name="optradio"]:checked').val();
+  $scope.request.result = $('input[name="optradio1"]:checked').val();
+  $scope.request.implemented = $('input[name="optradio2"]:checked').val();
+  $scope.request.duration = $('input[name="optradio3"]:checked').val();
+  $scope.request.hard_soft = $('input[name="optradio4"]:checked').val();
+  $scope.request.outage = $('input[name="optradio5"]:checked').val();
+  $scope.request.test = $('input[name="optradio6"]:checked').val();
+  $scope.request.SLA = $('input[name="optradio7"]:checked').val();
+  $scope.request.start_date = document.getElementById("date1").value
+  $scope.request.end_date = document.getElementById("date2").value
+  $scope.request.rt= $( "#r option:selected" ).map(function(){ return $(this).text();}).get().join();
+  $scope.request.nt= $( "#t option:selected" ).map(function(){ return $(this).text();}).get().join();
+  $scope.request.categories = $( "#c option:selected" ).map(function(){ return $(this).text();}).get().join();
 
-          if($scope.request.subject != undefined){
+  if($scope.request.subject != undefined){
 
 var json = JSON.stringify($scope.request,null, 4);
 
-        RFC_factory.create(json)
+        RFC_factory.create_form(json)
           .success(function(data){
 
               $scope.request = {};
 
           });
       }
+      else {toastr.error('Please fill in required fields!','Error',{
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      });
+    }
+              window.top.location = "options.html";
   };
   $scope.t_login = function(){
       RFC_factory.connectTwitter()
